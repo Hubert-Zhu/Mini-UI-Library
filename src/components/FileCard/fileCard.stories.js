fde2d2d555cae2ac4bbf058269ecee48c0f4109d
Hubert Zhu <zhubolin010118@gmail.com>
2020-08-10 17:59:38 -0400

    FileCard

---
+++ src/components/FileCard/fileCard.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import FileCard from ".";
+
+export default {
+    title: "UI Components/FileCard",
+    component: "FileCard"
+};
+
+export const Default = () => <FileCard />
