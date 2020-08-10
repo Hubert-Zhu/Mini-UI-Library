e9762e6bafa784e9a7fcd7d84ac776ba9256ce32
Hubert Zhu <zhubolin010118@gmail.com>
2020-08-10 14:39:02 -0400

    BlcokedList

---
+++ src/components/BlockedList/blockedList.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import BlockedList from ".";
+
+export default {
+    title: "Page Components/BlockedList",
+    component: "BlockedList"
+};
+
+export const Default = () => <BlockedList />
---
+++ src/components/BlockedList/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledBlockedList, {
+  SettingsMenu,
+  ClosableAvatar,
+  BlockedAvatar,
+  BlockedName,
+  CloseIcon,
+  FriendList,
+} from "./style";
+import { ReactComponent as ArrowMenuLeft } from "assets/icon/arrowMenuLeft.svg";
+import { ReactComponent as closeCircle } from "assets/icon/closeCircle.svg";
+import face from "assets/images/face-male-1.jpg";
+import "styled-components/macro";
+import Icon from "components/Icon";
+import Text from "components/Text";
+
+function BlockedList({ children, ...rest }) {
+  return (
+    <StyledBlockedList {...rest}>
+      <SettingsMenu>
+        <Icon
+          icon={ArrowMenuLeft}
+          css={`
+            cursor: pointer;
+          `}
+        />
+        <Text size="xxlarge">Black List</Text>
+      </SettingsMenu>
+      <FriendList>
+        {new Array(8).fill(0).map((item, index) => (
+          <ClosableAvatar key={index}>
+            <BlockedAvatar size="105px" src={face} />
+            <CloseIcon width={46} height={51} icon={closeCircle} />
+            <BlockedName>???</BlockedName>
+          </ClosableAvatar>
+        ))}
+      </FriendList>
+    </StyledBlockedList>
+  );
+}
+
+BlockedList.propTypes = {
+  children: PropTypes.any,
+};
+
+export default BlockedList;
