04d5e83ab351ede09dfe38366bbccad242fdf719
Hubert Zhu <zhubolin010118@gmail.com>
2020-08-14 10:54:56 -0400

    Update

--- src/components/ChatAPP/style.js
+++ src/components/ChatAPP/style.js
@@ -1,49 +1,54 @@
 import styled, { css } from "styled-components";
 
 const Nav = styled.nav`
-  flex-shrink: 0;
+    flex-shrink: 0;
 
-  position: relative;
-  z-index: 100;
+    position: relative;
+    z-index: 100;
 `;
 
 const Sidebar = styled.aside`
-  max-width: 448px;
-  min-width: 372px;
-  height: 100vh;
-  flex: 1;
-  background: ${({ theme }) => theme.grediantGray};
+    max-width: 448px;
+    min-width: 372px;
+    height: 100vh;
+    flex: 1;
+    background: ${({ theme }) => theme.grediantGray};
 
-  position: relative;
-  z-index: 50;
-  & > div {
-    will-change: transform. opacity;
-    position: absolute;
-    width: 100%;
-  }
+    position: relative;
+    z-index: 50;
+    & > div {
+        will-change: transform. opacity;
+        position: absolute;
+        width: 100%;
+    }
 `;
 
 const Content = styled.main`
-  flex: 2;
-  position: relative;
+    flex: 2;
+    position: relative;
 `;
 
 const Drawer = styled.div`
-  max-width: 310px;
-  width: 0;
-  ${({ show }) =>
-    show &&
-    css`
-      width: 310px;
-    `}
+    max-width: 310px;
+    width: 0;
+    transform: translateX(200px);
+    transition: transform 0.4s;
+    will-change: width, transform;
+
+    ${({ show }) =>
+        show &&
+        css`
+            width: initial;
+            transform: translateX(0px);
+        `}
 `;
 
 const StyledChatAPP = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-  position: relative;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    position: relative;
 `;
 
 export default StyledChatAPP;
--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import StyledSettings, {
-  StyledSettingsItem,
-  SettingItemControl,
-  StyledSettingsGroup,
+    StyledSettingsItem,
+    SettingItemControl,
+    StyledSettingsGroup,
 } from "./style";
 import { ReactComponent as ArrowMenuRight } from "assets/icon/arrowMenuRight.svg";
 import Switch from "components/Switch";
@@ -12,79 +12,97 @@ import Seperator from "components/Seperator";
 import Icon from "components/Icon";
 import { Link } from "react-router-dom";
 import { css } from "styled-components/macro";
+import { useSpring, animated } from "react-spring";
 
 function Settings({ children, ...rest }) {
-  return (
-    <StyledSettings {...rest}>
-      <SettingsGroup groupName="Privacy Setting">
-        <SettingsItem label={"Need Verfication Before Following Friends"} />
-        <SettingsItem
-          label={"Suggestions For You"}
-          description="According to your Friend List, system will suggest some friends to you"
-        />
-        <SettingsItem label={"Only can find me by phone number"} />
-      </SettingsGroup>
-      <SettingsGroup groupName="Notification Setting">
-        <SettingsItem label={"New Message Notification"} />
-        <SettingsItem label={"Voice or Video Call Notification"} />
-        <SettingsItem label={"Show Notification Detail"} />
-        <SettingsItem label={"Voice"} />
-        <Link
-          to="/settings/blocked"
-          css={`
-            text-decoration: none;
-            color: inherit;
-          `}
-        >
-          <SettingsItem label={"Check Muted Friend List"} type="menu" />
-        </Link>
-      </SettingsGroup>
-    </StyledSettings>
-  );
+    const animeProps = useSpring({
+        transform: "translate3d(0px,0px,0px)",
+        opacity: 1,
+        from: { transform: "translate3d(100px, 0px, 0px)", opacity: 0 },
+        config: {
+            tension: 140,
+        },
+        delay: 300,
+    });
+
+    return (
+        <StyledSettings {...rest}>
+            <animated.div style={animeProps}>
+                <SettingsGroup groupName="Privacy Setting">
+                    <SettingsItem
+                        label={"Need Verfication Before Following Friends"}
+                    />
+                    <SettingsItem
+                        label={"Suggestions For You"}
+                        description="According to your Friend List, system will suggest some friends to you"
+                    />
+                    <SettingsItem label={"Only can find me by phone number"} />
+                </SettingsGroup>
+                <SettingsGroup groupName="Notification Setting">
+                    <SettingsItem label={"New Message Notification"} />
+                    <SettingsItem label={"Voice or Video Call Notification"} />
+                    <SettingsItem label={"Show Notification Detail"} />
+                    <SettingsItem label={"Voice"} />
+                    <Link
+                        to="/settings/blocked"
+                        css={`
+                            text-decoration: none;
+                            color: inherit;
+                        `}
+                    >
+                        <SettingsItem
+                            label={"Check Muted Friend List"}
+                            type="menu"
+                        />
+                    </Link>
+                </SettingsGroup>
+            </animated.div>
+        </StyledSettings>
+    );
 }
 
 function SettingsGroup({ groupName, children, ...rest }) {
-  return (
-    <StyledSettingsGroup>
-      <Paragraph size="xxlarge" style={{ paddingBottom: "24px" }}>
-        {groupName}
-      </Paragraph>
-      {children}
-    </StyledSettingsGroup>
-  );
+    return (
+        <StyledSettingsGroup>
+            <Paragraph size="xxlarge" style={{ paddingBottom: "24px" }}>
+                {groupName}
+            </Paragraph>
+            {children}
+        </StyledSettingsGroup>
+    );
 }
 
 export function SettingsItem({
-  type = "switch",
-  label,
-  description,
-  children,
-  ...rest
+    type = "switch",
+    label,
+    description,
+    children,
+    ...rest
 }) {
-  return (
-    <StyledSettingsItem {...rest}>
-      <SettingItemControl>
-        <Paragraph size="large">{label}</Paragraph>
-        {type === "switch" && <Switch />}
-        {type === "menu" && <Icon icon={ArrowMenuRight} />}
-      </SettingItemControl>
+    return (
+        <StyledSettingsItem {...rest}>
+            <SettingItemControl>
+                <Paragraph size="large">{label}</Paragraph>
+                {type === "switch" && <Switch />}
+                {type === "menu" && <Icon icon={ArrowMenuRight} />}
+            </SettingItemControl>
 
-      {description && (
-        <Paragraph type="secondary" style={{ margin: "4px 0" }}>
-          {description}
-        </Paragraph>
-      )}
+            {description && (
+                <Paragraph type="secondary" style={{ margin: "4px 0" }}>
+                    {description}
+                </Paragraph>
+            )}
 
-      <Seperator style={{ marginTop: "8px", marginBottom: "20px" }} />
-    </StyledSettingsItem>
-  );
+            <Seperator style={{ marginTop: "8px", marginBottom: "20px" }} />
+        </StyledSettingsItem>
+    );
 }
 
 Settings.propTypes = {
-  type: PropTypes.string,
-  label: PropTypes.string,
-  description: PropTypes.string,
-  children: PropTypes.any,
+    type: PropTypes.string,
+    label: PropTypes.string,
+    description: PropTypes.string,
+    children: PropTypes.any,
 };
 
 export default Settings;
